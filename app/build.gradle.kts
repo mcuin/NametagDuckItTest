d3bd2bd306589b004c6f8f33fe5ee6f2da73ec69
mykalcuin <mykcuin@gmail.com>
2025-01-23 13:39:00 -0700

    Setup for retrofit dependency injection module.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -60,6 +61,8 @@ dependencies {
     implementation(libs.retrofit.converter.serialization)
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.kotlin.serialization)
+    implementation(libs.okhttp3)
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
     androidTestImplementation(libs.androidx.junit)
---
+++ app/src/main/java/com/nametag/nametagduckittest/utils/RetrofitModule.kt
@@ -0,0 +1,31 @@
+package com.nametag.nametagduckittest.utils
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    fun provideBaseUrl() = "https://nametag-duckit-2.uc.r.appspot.com/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(baseUrl: String) = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(
+            Json.asConverterFactory("application/json".toMediaType())
+        )
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit) = retrofit.create(APIService::class.java)
+}
