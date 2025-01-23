840593cfdc2afd3a79a6be8c806e4de772a77398
mykalcuin <mykcuin@gmail.com>
2025-01-23 13:00:29 -0700

    Setup for Dagger.

---
+++ app/src/main/java/com/nametag/nametagduckittest/NametagDuckItTestApplication.kt
@@ -0,0 +1,10 @@
+package com.nametag.nametagduckittest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Required application class for getting hilt to work.
+ */
+@HiltAndroidApp
+class NametagDuckItTestApplication: Application()
